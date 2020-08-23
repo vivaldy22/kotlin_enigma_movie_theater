9a901a61dd70559e128a827d6c5ea7b143ea73da
Vivaldy Andhira <dididlav@gmail.com>
2020-08-23 20:32:18 +0700

    first commit

---
+++ app/src/main/java/com/example/enigma_movie_theater/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.enigma_movie_theater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
