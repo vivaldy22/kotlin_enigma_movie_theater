8e4de0a5a5c6f83245fc5958f56759dcea47cf35
Vivaldy Andhira <dididlav@gmail.com>
2020-08-23 22:59:07 +0700

    gridview done

--- app/src/main/java/com/example/enigma_movie_theater/domains/movie/MovieGridAdapter.kt
+++ app/src/main/java/com/example/enigma_movie_theater/domains/movie/MovieGridAdapter.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.navigation.Navigation
 import com.example.enigma_movie_theater.R
 import com.squareup.picasso.Picasso
 
@@ -29,25 +27,24 @@ class MovieGridAdapter(private val moviesList: List<Movie>, private val context:
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val inflater = parent?.context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(R.layout.item_layout_movie, null)
+        val movieCover = view.findViewById<ImageView>(R.id.img_grid_view_movie_cover)
 
-        if (convertView != null) {
-            val holder = ItemHolder(convertView)
-            Picasso.get().load(moviesList[position].image_url).into(holder.movieCover)
-            holder.title = moviesList[position].title
+        Picasso.get().load(moviesList[position].image_url).into(movieCover)
 
-            holder.movieCover.setOnClickListener {
-                println("${holder.title}. CLICKED")
+        movieCover.setOnClickListener {
+            println("${moviesList[position].title}. CLICKED")
 //            Navigation.findNavController(it)
 //                .navigate()
-            }
         }
 
-        return view
+        return view!!
     }
 
-    class ItemHolder(v: View) {
-        var title = ""
-        var movieCover: ImageView = v.findViewById(R.id.img_grid_view_movie_cover)
-    }
+}
+
+class ItemHolder(v: View) {
+
+    var title = ""
+    var movieCover: ImageView = v.findViewById(R.id.img_grid_view_movie_cover)
 
 }
